4b71c2f057ae61485ae183ea00ed8c2449dfc02c
hoodboy <littlepark0107@gmail.com>
2022-01-15 13:59:53 +0900

    feat: none blocking code

--- BlockingCode/main.js
+++ BlockingCode/main.js
@@ -1,8 +1,11 @@
 var fs = require("fs");
 
+// var data = fs.readFileSync('input.txt'); blocking code
+
 var data = fs.readFile('input.txt', function (err, data) {
     if (err) return console.error(err);
     console.log(data.toString());
 });
 
+// console.log(data.toString()); blocking code
 console.log("Program hs ended");
